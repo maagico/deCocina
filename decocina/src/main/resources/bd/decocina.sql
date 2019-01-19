362f607a7f8a8ecf3d1e0959abe1c7b99d0a68f9
miguelangel <miguelangel@192.168.1.34>
2019-01-19 02:59:09 +0100

    Prueba 2!!

--- decocina/src/main/resources/bd/decocina.sql
+++ decocina/src/main/resources/bd/decocina.sql
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `decocina` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `decocina`;
-
+ 
 -- --------------------------------------------------------
 
 --
